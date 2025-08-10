51898b8383fa514e24dbd266c5e4c13d344312ef
hisabellippel <hisabel_lippel@estudante.sesisenai.org.br>
2025-08-10 10:58:18 -0300

    Update read.php

--- read.php
+++ read.php
@@ -1,15 +1,42 @@
 <?php
 // Listagem com erro de lógica (ordem incorreta e falta de conexão)
-include("conexao.php");
+include 'db.php';
 
-$sql = "SELECT * FORM usuarios"; // Erro de SQL: FORM ao invés de FROM
-$resultado = mysqli_query($conn, $sql);
+$sql = "SELECT * FROM usuarios"; // Erro de SQL: FORM ao invés de FROM
 
-echo "<h1>Lista de Usuários</h1>";
+$res = mysqli_query($conn, $sql);
 
-while ($linha = mysqli_fetch_array($resultado)) {
-    echo "Nome: " . $linha['nome'] . "<br>";
-    echo "Email: " . $linha['email'] . "<br><br>";
+if ($result->num_rows > 0) {
+
+    echo "<table border ='1'>
+        <tr>
+            <th> ID </th>
+            <th> Nome </th>
+            <th> Email </th>
+            <th> Data de Criação </th>
+            <th> Ações </th>
+        </tr>
+         ";
+
+while ($row = $res->fetch_assoc()) {
+    echo "<tr>
+                <td> {$row['id']} </td>
+                <td> {$row['name']} </td>
+                <td> {$row['email']} </td>
+                <td> {$row['created_at']} </td>
+                <td> 
+                    <a href='update.php?id={$row['id']}'>Editar<a>
+                    <a href='delete.php?id={$row['id']}'>Excluir<a>
+                
+                </td>
+              </tr>   
+        ";
+    }
+    echo "</table>";
+} else {
+    echo "Nenhum registro encontrado.";
 }
-?>
-<a href='cadastrar.php'>Cadastrar novo</a>
+
+$conn -> close();
+
+echo "<a href='create.php'>Inserir novo Registro</a>";
